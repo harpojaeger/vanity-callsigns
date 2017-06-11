d7a96c163432a4b8e67ed5edb704365fa1fef33a
Harpo Jaeger <harpo.jaeger@gmail.com>
2017-06-10 17:11:12 -0700

    Commit everything else so Heroku doesn't whine

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const bodyParser = require('body-parser')
+require('dotenv').config()
+const port = process.env.PORT
+
+app.use( bodyParser.json() )
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
+if (process.env.NODE_ENV === 'production') {
+  console.log('production mode')
+  app.use(express.static('client/build'));
+}
+
+app.get('/', (req, res) => {
+  res.send('Hello world.')
+})
+
+app.listen(port, () => {
+  console.log('Server running on port', port)
+})
+
+module.exports = app
