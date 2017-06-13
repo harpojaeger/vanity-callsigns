096c54532246bba107d6e062058b98d64e090d95
Harpo Jaeger <harpo.jaeger@gmail.com>
2017-06-12 21:13:02 -0700

    Is this why I can only make one query before it crashes?

--- search.js
+++ search.js
@@ -36,7 +36,6 @@ router.get('/', (req, res) => {
     .then( (rows) => {
       console.log('found',rows.length,'callsigns matching', req.query.s)
       res.send(rows)
-      knex.destroy()
     })
     .catch( (err) => {
       console.error(err)
