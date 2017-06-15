27f5be31b07150741a9811e9c7abeaf5159fed1d
Harpo Jaeger <harpo.jaeger@gmail.com>
2017-06-14 20:08:34 -0700

    Refactor <ResultsList /> into a class-based component

--- client/src/components/ResultsList.js
+++ client/src/components/ResultsList.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import Callsign from './Callsign'
 import '../style/ResultsList.css'
@@ -24,28 +24,41 @@ function ResultsGroup(props) {
   )
 }
 
-function ResultsList(props) {
-  function byDimension(callsign) {
-    return callsign.prefix.length === this.prefixLength && callsign.suffix.length === this.suffixLength
+class ResultsList extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      // filterParams: defaultFilterState
+    }
+    this.byDimension = this.byDimension.bind(this)
   }
-  // Split the callsigns up into grops by dimension.
-  const oneByTwos = props.results.filter(byDimension, {prefixLength: 1, suffixLength: 2})
-  const oneByThrees = props.results.filter(byDimension, {prefixLength: 1, suffixLength: 3})
-  const twoByOnes = props.results.filter(byDimension, {prefixLength: 2, suffixLength: 1})
-  const twoByTwos = props.results.filter(byDimension, {prefixLength: 2, suffixLength: 2})
-  const twoByThrees = props.results.filter(byDimension, {prefixLength: 2, suffixLength: 3})
+  byDimension(callsign) {
+    return (callsign, filterParams) => {
+      return callsign.prefix.length === filterParams.prefixLength && callsign.suffix.length === filterParams.suffixLength
+    }
+  }
 
-  // const sumOfSplitLengths = oneByTwos.length + oneByThrees.length + twoByOnes.length + twoByTwos.length + twoByThrees.length
-  // console.log('Split callsigns, length sum is',sumOfSplitLengths,'total length is',props.results.length)
-  return (
-    <div>
-      <ResultsGroup title='1x2s' results={oneByTwos} />
-      <ResultsGroup title='2x1s' results={twoByOnes} />
-      <ResultsGroup title='1x3s' results={oneByThrees} />
-      <ResultsGroup title='2x2s' results={twoByTwos} />
-      <ResultsGroup title='2x3s' results={twoByThrees} />
-    </div>
-  )
+  render(){
+    // Split the callsigns up into grops by dimension.
+    const oneByTwos = this.props.results.filter(this.byDimension({prefixLength: 1, suffixLength: 2}))
+    const oneByThrees = this.props.results.filter(this.byDimension, {prefixLength: 1, suffixLength: 3})
+    const twoByOnes = this.props.results.filter(this.byDimension, {prefixLength: 2, suffixLength: 1})
+    const twoByTwos = this.props.results.filter(this.byDimension, {prefixLength: 2, suffixLength: 2})
+    const twoByThrees = this.props.results.filter(this.byDimension, {prefixLength: 2, suffixLength: 3})
+
+    // const sumOfSplitLengths = oneByTwos.length + oneByThrees.length + twoByOnes.length + twoByTwos.length + twoByThrees.length
+    // console.log('Split callsigns, length sum is',sumOfSplitLengths,'total length is',this.props.results.length)
+    return (
+      <div>
+        <ResultsGroup title='1x2s' results={oneByTwos} />
+        <ResultsGroup title='2x1s' results={twoByOnes} />
+        <ResultsGroup title='1x3s' results={oneByThrees} />
+        <ResultsGroup title='2x2s' results={twoByTwos} />
+        <ResultsGroup title='2x3s' results={twoByThrees} />
+      </div>
+    )
+  }
+
 }
 
 ResultsGroup.propTypes = {
