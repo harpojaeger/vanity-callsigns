cb86782e1e5456b75fe68bd06e4a6050b777036d
Harpo Jaeger <harpo.jaeger@gmail.com>
2017-09-17 15:53:33 -0700

    Output license info using shortname dictionary

--- client/src/components/CallsignDetails.js
+++ client/src/components/CallsignDetails.js
@@ -1,7 +1,21 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import {Button, FormControl, FormGroup }  from 'react-bootstrap'
 import api from '../utils/api'
 import '../style/CallsignDetails.css'
+
+const callsignAttributeDictionary = {
+  callsign: 'Callsign',
+  license_status: 'License status',
+  effective_date: 'Effective date',
+  cancellation_date: 'Cancellation date',
+  expired_date: 'Expiration date',
+  certifier_first_name: 'Licenseholder first name',
+  certifier_mi: 'Licenseholder middle initial',
+  certifier_last_name: 'Licenseholder last name',
+  certifier_suffix: 'Licenseholder suffix',
+  grant_date: 'Grant date',
+}
 
 class CallsignDetails extends Component {
   constructor(props) {
@@ -15,8 +29,9 @@ class CallsignDetails extends Component {
   runSearch(e) {
     e.preventDefault()
     const callsignRegex = /^(?=.{4,6}$)^((?:A[A-K])|(?:[WKN][A-Z]?))[0-9][A-Z]{1,3}$/i
-    console.log('is',this.state.query,'a valid callsign:',callsignRegex.test(this.state.query))
-    this.callsignLookup(this.state.query)
+    const isValidCallsign = callsignRegex.test(this.state.query)
+    console.log('is',this.state.query,'a valid callsign:', isValidCallsign)
+    if(isValidCallsign) this.callsignLookup(this.state.query)
   }
   callsignLookup(callsign) {
     api.bulkSearch([callsign.toUpperCase()])
@@ -40,13 +55,28 @@ class CallsignDetails extends Component {
             <Button type='submit' value='search' onClick={this.runSearch}>Lookup</Button>
           </FormGroup>
         </form>
+        {this.state.hasOwnProperty('res') &&
+          <ul>
+            {
+              Object.entries(this.state.res[0]).map( ([key, value]) => {
+                return <CallsignAttribute key={key} shortname={key} fullname={callsignAttributeDictionary[key]} value={value} />
+              })
+            }
+          </ul>
+        }
       </div>
     )
   }
 }
 
 function CallsignAttribute(props) {
+  return <li key={props.shortname}>{props.fullname}: {props.value}</li>
+}
 
+CallsignAttribute.propTypes = {
+  shortname: PropTypes.string.isRequired,
+  fullname: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
 }
 
 export default CallsignDetails
